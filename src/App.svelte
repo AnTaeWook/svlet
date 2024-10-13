0adcce69d7b4c6534ce07f43f81cbd3ffb9f54ab
안태욱 <antk7894@naver.com>
2024-10-13 18:47:44 +0900

    init project

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  const data = [
+    {
+      title: "고질라 X 콩: 뉴 엠파이어",
+      year: 2024,
+      category: "어드벤처, 액션, SF",
+      likeCount: 0,
+    },
+    {
+      title: "듄: 파트2",
+      year: 2024,
+      category: "액션",
+      likeCount: 0,
+    }
+  ];
+
+  const handleLike = ({index}) => {
+    data[index].likeCount += 1;
+  };
+</script>
+
+<main>
+    <h1>영화정보</h1>
+    {#each data as item, i}
+        <div>
+            <h3 class="bg-yellow">{item.title}</h3>
+            <p>개봉: {item.year}</p>
+            <p>장르: {item.category}</p>
+            <button on:click={
+                () => {handleLike({index: i})}
+            }>좋아요 {item.likeCount}</button>
+        </div>
+    {/each}
+</main>
+
+<style>
+    .bg-yellow {
+        background-color: gold;
+        padding: 10px;
+        color: black;
+    }
+</style>
